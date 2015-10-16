de1e0dbec22a08c4e78ca1a2de93cb7cc2a58169
Paulo Faria <paulo.faria.rl@gmail.com>
2015-10-15 23:34:24 -0300

    Initial Commit

---
+++ HTTPParser/HTTPParser.h
@@ -0,0 +1,19 @@
+//
+//  HTTPParser.h
+//  HTTPParser
+//
+//  Created by Paulo Faria on 10/15/15.
+//  Copyright © 2015 Zewo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTTPParser.
+FOUNDATION_EXPORT double HTTPParserVersionNumber;
+
+//! Project version string for HTTPParser.
+FOUNDATION_EXPORT const unsigned char HTTPParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPParser/PublicHeader.h>
+
+
