381a756ee9f5c74f4dcb68f603701d4569f56417
Paulo Faria <paulo.faria.rl@gmail.com>
2015-10-18 13:37:49 -0200

    add RawURI

---
+++ HTTPParser/RawURI.swift
@@ -0,0 +1,53 @@
+// RawURI.swift
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Zewo
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import http_parser
+
+public struct RawURI {
+    public let scheme: String?
+    public let host: String?
+    public let port: Int?
+    public let path: String?
+    public let query: String?
+    public let fragment: String?
+    public let userInfo: String?
+}
+
+extension RawURI {
+    init(uri: parsed_uri) {
+        scheme   = String.fromCString(uri.scheme)
+        host     = String.fromCString(uri.host)
+        port     = (uri.port != nil) ? Int(uri.port.memory) : nil
+        path     = String.fromCString(uri.path)
+        query    = String.fromCString(uri.query)
+        fragment = String.fromCString(uri.fragment)
+        userInfo = String.fromCString(uri.user_info)
+    }
+}
+
+extension RawURI {
+    public init(string: String) {
+        self = RawURI(uri: parse_uri(string))
+    }
+}
