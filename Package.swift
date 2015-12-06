c76456eb7f9c929417ebd3928345e8450b43c860
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-06 12:48:31 -0200

    add package

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+	name: "HTTPParser",
+	dependencies: [
+        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1)
+	]
+)
