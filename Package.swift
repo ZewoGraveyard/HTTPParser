a30cc073b32a03134b6126b3bb730159013f036b
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 06:55:04 -0800

    fix indentation

--- Package.swift
+++ Package.swift
@@ -6,5 +6,5 @@ let package = Package(
         .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
-	]
+    ]
 )
