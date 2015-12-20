98f3c9c8a89125a99e429d3ff1c52e8397f515d1
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 06:41:38 -0800

    make Package file explicit

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
 	name: "HTTPParser",
 	dependencies: [
         .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1)
 	]
 )
