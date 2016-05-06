1cdef99760df50458ba4ccb72f74cbf8d8d9f621
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-05-06 17:44:01 +0900

    Read me change

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
 	name: "HTTPParser",
 	dependencies: [
         .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/tomohisa/URI.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 6),
     ]
 )
