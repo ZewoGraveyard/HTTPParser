20842bb0cb8485630a06c62d6c82eb42d0db6fa7
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-06-03 15:21:18 +0200

    Updated Swift and S4

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
 	dependencies: [
         .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/Zewo/URI.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 9),
     ]
 )
